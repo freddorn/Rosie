a1f95217f3ab55056e58f3c530b942529e95eb79
Ubuntu <ubuntu@ip-172-31-38-58.ec2.internal>
2019-07-01 18:34:58 +0000

    change email back

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,9 +1,16 @@
-var template_params = {
-   "name": "name_value",
-   "email": "email_value",
-   "message": "message_value"
+function sendMail(contactForm) {
+    emailjs.send("gmail", "rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
 }
-
-var service_id = "default_service";
-var template_id = "vacation";
-emailjs.send(service_id, template_id, template_params);
